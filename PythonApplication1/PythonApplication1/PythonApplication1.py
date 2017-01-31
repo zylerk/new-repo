ce85ec14865414ec4b8d4f65efaf9aee2d0d3ab6
zylerk <zylerk@gmail.com>
2017-02-01 01:06:04 +0900

    aa

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -6,14 +6,70 @@ import module1
 
 print("this is start.")
 
-
-
-
-
 movies = ["the holy grail", "the life of brian", "the meaning  of life", ["nested item", ["again item"]]]
 
+
+import os
+print(os.getcwd())
+
 #print_list(movies)
-module1.print_list (movies)
+#module1.print_list (movies)
+
+
+data = open("sketch.txt")
+data.seek(0)
+
+man = []
+other = []
+
+for each_line in data:
+    try:
+        (l_value, r_value) = each_line.split(":", 1)
+        #print( l_value, end='')
+        #print( " said: ", end="")
+        #print( r_value, end='')
+
+        r_value.strip()
+
+        if( l_value == 'Man'):
+            man.append(r_value)
+        elif (l_value == "Other Man"):
+            other.append(r_value)
+
+
+    except:
+        print(" exception:", each_line)
+       
+data.close()
+
+try:
+    out = open("data.out", "w")
+    
+    print("gogogo", file=out)
+    print(man, file=out)
+    print(other, file=out)
+
+    
+
+except IOError:
+    pass
+
+finally:
+    out.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
